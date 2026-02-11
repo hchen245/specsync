60a33791c5cd3b17b7f0f6795a4b117bd969b1ba
Anastacio <92560533+anastar99@users.noreply.github.com>
2026-02-10 16:21:34 -0800

    Add vite frontend basic boiler plate code

--- src/frontend/specsync-frontend/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import "./App.css";
-const API_URL = import.meta.env.VITE_API_BASE_URL;
-
-function App() {
-  return <h1>Hello this is specsync, our api url is {API_URL}</h1>;
-}
-
-export default App;
